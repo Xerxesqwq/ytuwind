dceec1c5e4d227a07c44676196aadc58cd7aeec1
virtualman <virtualman@yeah.net>
2020-10-31 09:23:33 +0800

    2020/10/31

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 
 # 打开数据库连接
 db = pymysql.connect("localhost", "ytuwind", "XC4djtPwCDjsfGZG", "ytuwind", charset='utf8' )
+
+'''
+方法名：SendSQL()
+功能：执行SQL语句并返回执行结果
+'''
 def SendSQL(sql):
     cursor = db.cursor()
     cursor.execute(sql)
@@ -15,11 +20,26 @@ def SendSQL(sql):
     # for row in results:
     cursor.close()
     return results
+
+'''
+方法名：RegisteredUsers()
+功能：注册账号
+成功返回 注册的账户ID
+失败返回-1
+ID默认为6位
+'''
 def RegisteredUsers(username,password,realname,studentnum,college,major,headimageurl,phonenum,classnum):
     sql = "INSERT INTO `ytuwind`.`yw_users`(`username`, `password`, `realname`, `studentsnum`, `college`, `major`, `headimageurl`, `phonenum`, `classnum`) " \
           "VALUES ("+username+", '"+password+"', '"+realname+"', '"+studentnum+"', '"+college+"', '"+major+"', '"+headimageurl+"', '"+phonenum+"', "+classnum+")"
+    res = SendSQL(sql)
+    if res == 1:
+        print("注册成功")
+        return id
+    if res == -1:
+        print("注册失败")
+        return -1
 
-    return id
+
 
 
 @app.route('/')
@@ -38,4 +58,4 @@ def UserId(id):
     return render_template('user.html',**locals())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="192.168.3.32")
